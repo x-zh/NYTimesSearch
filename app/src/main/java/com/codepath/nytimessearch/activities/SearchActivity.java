3766b7efef9976e391ad2c45f414c786dc0e0632
Charlie Zhou <charlie.zhou@airbnb.com>
2016-05-26 23:07:13 -0700

    add action bar for search view

--- app/src/main/java/com/codepath/nytimessearch/activities/SearchActivity.java
+++ app/src/main/java/com/codepath/nytimessearch/activities/SearchActivity.java
@@ -1,10 +1,16 @@
 package com.codepath.nytimessearch.activities;
 
 import android.support.design.widget.Snackbar;
+import android.support.v4.view.MenuItemCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.support.v7.widget.GridLayoutManager;
 import android.support.v7.widget.RecyclerView;
+import android.support.v7.widget.SearchView;
+import android.support.v7.widget.Toolbar;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
 import android.widget.EditText;
 import android.widget.RelativeLayout;
 
@@ -30,22 +36,22 @@ public class SearchActivity extends AppCompatActivity {
     @BindView(R.id.searchLayout)
     RelativeLayout searchLayout;
 
-    @BindView(R.id.etSearch)
-    EditText etSearch;
-
     @BindView(R.id.rvArticles)
     RecyclerView rvArticles;
+
+    @BindView(R.id.tbSearch)
+    Toolbar tbSearch;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_seach);
+        setContentView(R.layout.activity_search);
         ButterKnife.bind(this);
+        setSupportActionBar(tbSearch);
     }
 
-    @OnClick(R.id.btnSearch)
-    public void search() {
-        restClient.getApiService().searchArticle(etSearch.getText().toString()).enqueue(new Callback<List<Article>>() {
+    public void search(String query) {
+        restClient.getApiService().searchArticle(query).enqueue(new Callback<List<Article>>() {
             @Override
             public void onResponse(Call<List<Article>> call, Response<List<Article>> response) {
                 ArticleAdapter articleAdapter = new ArticleAdapter(response.body());
@@ -63,5 +69,32 @@ public class SearchActivity extends AppCompatActivity {
                         Snackbar.LENGTH_LONG).show();
             }
         });
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        MenuInflater inflater = getMenuInflater();
+        inflater.inflate(R.menu.main, menu);
+        MenuItem searchItem = menu.findItem(R.id.action_search);
+        final SearchView searchView = (SearchView) MenuItemCompat.getActionView(searchItem);
+        searchView.setOnQueryTextListener(new SearchView.OnQueryTextListener() {
+            @Override
+            public boolean onQueryTextSubmit(String query) {
+                // perform query here
+
+                // workaround to avoid issues with some emulators and keyboard devices firing twice if a keyboard enter is used
+                // see https://code.google.com/p/android/issues/detail?id=24599
+                searchView.clearFocus();
+                search(query);
+                return true;
+            }
+
+            @Override
+            public boolean onQueryTextChange(String newText) {
+                return false;
+            }
+        });
+        return super.onCreateOptionsMenu(menu);
+
     }
 }
